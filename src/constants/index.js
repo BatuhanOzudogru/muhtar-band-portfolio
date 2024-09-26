25b838f5e343f9ef01af75f8b4f609ef241a735c
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2024-09-27 02:28:19 +0300

    Add index.js

---
+++ src/constants/index.js
@@ -0,0 +1,205 @@
+import foto1 from "/public/pictures/foto21.jpg";
+import foto2 from "/public/pictures/foto22.jpg";
+import foto3 from "/public/pictures/foto29.jpg";
+import foto4 from "/public/pictures/foto24.jpg";
+
+export const HERO_CONTENT = `2022 yılında müzik hayatına başlayan Muhtar; Oğuz Kaan Civelek, Korhan Koçanalı, Onur Eylül Akçay, Uğur Can Kızılaltun, Mertcan Çetin üyelerinden oluşuyor. 2023, Temmuz'da çıkarmış oldukları 'İçimdekiler' isimli teklileriyle birlikte, şarkılarını insanlara ulaştırma hayallerinin ilk adımlarını atmış bulunmaktalar.`;
+
+export const ABOUT_TEXT = `Uğur Can Kızılaltun, Oğuz Kaan Civelek ve Korhan Koçanalı'nın kurduğu Muhtar grubu, Sertaç Özçetin ve Osman Karaca'nın katılmasıyla birlikte ilk halini aldı. Sahne ismi olarak Rhythm Zero'yu kullanan grup ilk sahnelerini 2021 sonlarına doğru almaya başladı. 2022 yılında isim değişikliğine gidip Muhtar ismini alan grup Mertcan Çetin ve Onur Eylül Akçay'ın katılmasıyla son halini aldı. 
+İlk singleları olan "İçimdekiler" ve "Maske" yi 2022 yılında çıkartan Muhtar, 2024 itibariyle albüm çalışmalarına devam edip aynı zamanda çeşitli organizasyonlarda sahne almaya devam ediyor.`;
+
+export const MEMBERS = [  
+ 
+  {
+    name: "Korhan Koçanalı",
+    role: "Baterist",
+    photo: "../../public/pictures/member1.jpg",
+    description: `1993 Bursa doğumlu olan Korhan, müziğe 16 yaşında bateri çalarak başladı. Ardından üniversite için gittiği Çanakkale'de yerel sanatçılarla ve gruplarla sahne alan Korhan Bursa'ya döndükten sonra Muhtar grubunun bateristi olarak müzik hayatına devam etmektedir.`
+
+  },
+  {
+    name: "Onur Eylül Akçay",
+    role: "Ritim Gitar",
+    photo: "../../public/pictures/member2.jpg",
+    description: `1996 yılı Bursa doğumlu olan Onur, müzik kariyerine 2006 yılında gitar çalarak başladı. Üniversiteyi Bursa Uludağ Üniversitesinde Makine Mühendisliğinde okuyan  Onur, UMT'nin çeşitli organizasyonlarında yer almasının yanında Bursa'da çeşitli gruplarla sahne deneyimi kazandı. 2022 yılında Muhtar grubuna elektro gitarist olarak katılan Onur ayrıca  prodüksiyon konusunda gruba destek olmaktadır.`
+  },
+  {
+    name: "Oğuz Kaan Civelek",
+    role: "Vokal",
+    photo: "../../public/pictures/member3.jpg",
+    description: `1997 yılı Bursa doğumlu olan Oğuz Kaan Civelek, müziğe 6 yaşında gitar çalarak başladı. İlk amatör grup deneyimini Bursa'da yaşayan Oğuz, üniversite okumak için gittiği İstanbul'da ilk profesyonel grup deneyimini yaşadı. Gitar haricinde ud, keman, bağlama ve klarnet çalan Oğuz ayrıca Muhtar grubunda vokallik yapmaktadır.`
+  },
+  {
+    name: "Uğur Can Kızılaltun",
+    role: "Solo Gitar",
+    photo: "../../public/pictures/member4.jpg",
+    description: `Müziğe 14 yaşında gitar çalarak başlayan Uğur Can, Bursa'da çeşitli gruplarla çalıştıktan sonra, Üniversite okumak için gittiği Eskişehir'de çeşitli gruplarla sahne alan Uğur üniversite sonrası döndüğü Bursa'da Muhtar grubundan önce de çeşitli gruplarla birlikte sahne aldı. Müzikal kariyerine Muhtar grubunda devam eden Uğur Can ayrıca prodüksiyon ile ilgilenmektedir.`
+  },
+  {
+    name: "Mertcan Çetin",
+    role: "Bas Gitar",
+    photo: "../../public/pictures/member5.jpg",
+    description: `1997 yılı Bursa doğumlu olan Mertcan, Müzik kariyerine 10 yaşında THM çocuk korosunda başladı, daha sonra Bursa'da çeşitli gruplarla ilk grup deneyimini kazandı. Bağlama, bass gitar ve perküsyon çalan Mertcan 2022 yılında katıldığı Muhtar grubunda bass gitar çalmasının yanında back vokallikte yapmaktadır.`
+  }
+];
+
+export const SONGS = [
+  {
+    year: "2024",
+    photo: "../../public/pictures/muhtarPic.jpg",
+    name: "Zaman Erir Biter",
+    type: "Single",
+    lyrics: `kaldık hasret mutlu yüzlere
+vardık yol alıp eski günlere
+ileriye gidemezsin derdinden
+vardır hep sarsar bir zelzele
+dün hissettiğin güzel hisler
+bugün kendini senden gizler
+gelir muhtar insanı dinler
+herkes bi noktada özüne döner
+zaman erir biter teker teker
+kuyusunu kazar insan, yiter
+vakit gelir bir gün ve rüzgar diner
+gam yüklü gemi ağır gider`,
+links: ["", "", "https://www.youtube.com/watch?v=7XFxfd8FKwM"],
+  },
+  {
+    year: "2024",
+    photo: "../../public/pictures/kazanKayniyor.jpg",
+    name: "Kazan Kaynıyor",
+    type: "Single",
+    lyrics: `Kazan kaynıyor, oluyor girdap
+Zaman geçiyor, çıkmıyor bir cevap
+İçine çekiyor istemesen de seni
+Ateş söner de ortada kalırsın son sap
+Yolun sonuna geldik artık durmamalı
+Yanlış kararlar hep sorgulanmalı
+Bu yer ki alır içine sonunda seni
+Doğrulara yaslanıp kurallar konmalı
+Bir sorun çıkar, çıkmadan olmaz
+Eski defterler fazla kurcalanmaz
+Bu alemin şakası yok sayılmaz
+El sonunda olanlar taht-el arza sığmaz
+Kulak versene, sen ve ben biz kimiz
+Tanışıklı dövüştük hep, aslında biriz
+Ağardı zihnim, çözülmedi düğüm
+Kazan kaynıyor, ateşi söndürün
+Kulak versene, sen ve ben biz kimiz
+Tanışıklı dövüştük hep, aslında biriz
+Ağardı zihnim, çözülmedi düğüm
+Kazan kaynıyor, ateşi söndürün
+Kulak versene, sen ve ben biz kimiz
+Tanışıklı dövüştük hep, aslında biriz
+Ağardı zihnim, çözülmedi düğüm
+Kazan kaynıyor, ateşi söndürün
+Kulak versene, sen ve ben biz kimiz
+Tanışıklı dövüştük hep, aslında biriz
+Ağardı zihnim, çözülmedi düğüm
+Kazan kaynıyor, ateşi söndürün
+Kazan kaynıyor artık ateşi söndürün
+Kazan kaynıyor artık ateşi söndürün
+Kazan kaynıyor artık ateşi söndürün`,
+    links: ["https://open.spotify.com/intl-tr/track/4PrTJxr8NtAY1NcF6spep2?si=76c3981e1d01468d", "https://www.youtube.com/watch?v=sW1uJWwqaBo","https://www.youtube.com/watch?v=526rFiaXOmI"],
+  },
+  
+  {
+    year: "2023",
+    photo: "../../public/pictures/icimdekiler.jpg",
+    name: "İçimdekiler",
+    type: "Single",
+    lyrics: `İnleye inleye dönen değirmenin kör atı
+Aman ha hiç kimseye muhtaç olmamalı
+Ölümü sol cebinde taşımadı, atmadı
+Kendi mezarını, kendi kazmadı
+Ah
+Kendime bir gelsem var ya
+Daha ne isterim bu hayattan
+Bu güneş can yakar, çat atı vurdular
+Dinledi yankısını, ah bütün avlular
+Dört nala üzerime, gelirken korkular
+İki kere ölmek gerektiğini atladılar
+Ah
+Kendime bir gelsem var ya
+Daha ne isterim bu hayattan
+Kendime bir gelsem var ya
+Daha ne isterim bu hayattan
+Ah bu benim içimdekiler
+Benim değil ama kime aitler
+Ah bu benim içimdekiler
+Benim değil ama kime aitler
+Kendime bir gelsem var ya
+Daha ne isterim, ben bu hayattan
+Kendime bir gelsem var ya
+Daha ne isterim, ben bu hayattan`,
+links: ["https://open.spotify.com/intl-tr/track/0NZcBQ3Au806gKW2ohWSWh?si=d49bc84569374924", "https://www.youtube.com/watch?v=qAZVMG2v7UA"],
+  },
+  
+  {
+    year: "2023",
+    photo: "../../public/pictures/maske.jpg",
+    name: "Maske",
+    type: "Single",
+    lyrics: `Düzen değişti, sen hep aynı
+Kuralların üstünü loş ışıklı fısıltılar aldı
+Yolda bulduğum ağdalı sırlar
+Açmadı eskimiş, paslı kapıları
+Kalmadı bu şehrin karalanacak yeri
+Takip ediyorum kullanılmış çizgileri
+Yetmiyor ki tüm şehri aydınlatmaya
+Kağıdın üstünde açtığım yara izleri
+Ve şimdi istiyorsan yak bu şehri
+Savrulup giderken içimdeki sırların külleri
+Aklına gelir miydi o gün taktığın maskenin rengi
+Aklına gelir miydi o gün taktığın maskenin rengi
+Alevler yükselirken şehrin sokaklarından
+Ne güzel gülümsedin vedanı ederken
+Yok olan tüm fısıltıların gözyaşları
+Boğar mıydı şehirden yükselen çığlıkları
+Ve şimdi istiyorsan yak bu şehri
+Savrulup giderken içimdeki sırların külleri
+Aklına gelir miydi o gün taktığın maskenin rengi
+Aklına gelir miydi o gün taktığın maskenin rengi
+Aklına gelir miydi o gün taktığın maskenin rengi
+Aklına gelir miydi o gün taktığın maskenin rengi`,
+links: ["https://open.spotify.com/intl-tr/track/4L6uhn4xhOZNeqIeZq2UQM?si=cfd1d87314c54440", "https://www.youtube.com/watch?v=kVS83HMXxQ0"],
+  },
+ 
+];
+
+export const CONCERTS = [
+  {
+    title: "A Konseri",
+    image: foto1,
+    description:
+      "a mekanında gerçekleşen konser",
+    
+    
+  },
+  {
+    title: "b Konseri",
+    image: foto2,
+    description:
+      "b mekanında gerçekleşen konser",
+    
+  },
+  {
+    title: "c Konseri",
+    image: foto3,
+    description:
+      "c mekanında gerçekleşen konser",
+    
+  },
+  {
+    title: "d Konseri",
+    image: foto4,
+    description:
+      "d mekanında gerçekleşen konser",
+    
+  },
+];
+
+export const CONTACT = {
+  address: "Bursa, Turkey",
+  phoneNo: "0531 676 19 08",
+  email: "muhtarmusicband@gmail.com ",
+};
